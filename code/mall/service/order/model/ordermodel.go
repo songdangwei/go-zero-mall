5c6a8621186242e37d3245dbd8fe48161735c61f
ziyansuz <ziyansuz@163.com>
2023-06-23 13:19:30 +0700

    add product & order

---
+++ code/mall/service/order/model/ordermodel.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"fmt"
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlc"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ OrderModel = (*customOrderModel)(nil)
+
+type (
+	// OrderModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customOrderModel.
+	OrderModel interface {
+		orderModel
+		FindAllByUid(uid int64) ([]*Order, error)
+	}
+
+	customOrderModel struct {
+		*defaultOrderModel
+	}
+)
+
+func (m *defaultOrderModel) FindAllByUid(uid int64) ([]*Order, error) {
+	var resp []*Order
+
+	query := fmt.Sprintf("select %s from %s where `uid` = ?", orderRows, m.table)
+	err := m.QueryRowsNoCache(&resp, query, uid)
+
+	switch err {
+	case nil:
+		return resp, nil
+	case sqlc.ErrNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+// NewOrderModel returns a model for the database table.
+func NewOrderModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) OrderModel {
+	return &customOrderModel{
+		defaultOrderModel: newOrderModel(conn, c, opts...),
+	}
+}
